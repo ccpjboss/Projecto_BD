cac6f2394ed7e23df046d9d6c90b758602907c2e
ccpjboss <33908859+ccpjboss@users.noreply.github.com>
2019-11-26 17:45:50 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import psycopg2
+import menu
+import funcoes
+
+funcoes.connect_db()
+print()
+menu.menu_inicial()
---
+++ menu.py
@@ -0,0 +1,7 @@
+def menu_inicial():
+    print("********************************")
+    print("*Welcome to Vynil Records Store*")
+    print("********************************")
+    print("1.\tLogin")
+    print("2.\tNovo ultilizador/Registo")
+    opcao = input("Insira a opção")
