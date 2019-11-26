b21db8b76429a0fd958af83bc4adb76bab45edb5
João Castilho <jpccastilho1999@gmail.com>
2019-11-26 22:54:38 +0000

    4

--- menu.py
+++ menu.py
@@ -4,4 +4,20 @@ def menu_inicial():
     print("********************************")
     print("1.\tLogin")
     print("2.\tNovo ultilizador/Registo")
-    opcao = input("Insira a opção")
+    opcao = int(input("Insira a opção"))
+
+    if opcao != 1 and opcao != 2:
+        print("Insira uma das opcões!")
+        while True:
+            opcao = int(input("Insira a opção"))
+            if opcao == 1 or opcao == 2:
+                break
+            print("Insira uma das opcões!")
+    if opcao == 1:
+        print("Login: ")
+        print("Insira o seu e-mail: ")
+
+    elif opcao == 2:
+        print("Registo: ")
+        user_email = input("Insira o seu email: ")
+        if user_email == NULL
