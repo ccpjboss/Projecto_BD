4c545feb3e6b301da426543b8426f70662a101e1
João Castilho <jpccastilho1999@gmail.com>
2019-11-27 01:23:13 +0000

    Registo concluido tabela cliente error

--- menu.py
+++ menu.py
@@ -1,26 +1,45 @@
+import funcoes
+
+
 def menu_inicial():
     print("********************************")
     print("*Welcome to Vynil Records Store*")
     print("********************************")
     print("1.\tLogin")
     print("2.\tNovo ultilizador/Registo")
-    opcao = int(input("Insira a opção"))
 
-    if opcao != 1 and opcao != 2:
-        print("Insira uma das opcões!")
-        while True:
-            opcao = int(input("Insira a opção"))
-            if opcao == 1 or opcao == 2:
-                break
-            print("Insira uma das opcões!")
+    while True:
+        opcao = int(input("Insira a opção: "))
+        if opcao not in [1, 2]:
+            print("Insira um opção valida!")
+        else:
+            break
+
     if opcao == 1:
         print("Login: ")
         print("Insira o seu e-mail: ")
 
     elif opcao == 2:
         print("Registo: ")
-        user_email = input("Insira o seu email: ")
-        if user_email == NULL
+        while True:
+            user_email = input("Insira o email: ")
+            if user_email == '':
+                print("Não insira um campo vazio!")
+            else:
+                break
+        while True:
+            user_password = input("Insira a passwd: ")
+            if user_password == '':
+                print("Não insira um campo vazio!")
+            else:
+                break
+        while True:
+            user_nome = input("Insira o seu nome: ")
+            if user_nome == '':
+                print("Não insira um campo vazio!")
+            else:
+                break
+    funcoes.novo_user(user_email, user_password, user_nome)
 
 
 def menu_cliente():
